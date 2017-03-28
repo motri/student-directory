3528a5b2e6b5ef35aa1cb9e0763ca3280d21687f
Unai Motriko <unaimotriko@gmail.com>
2017-03-28 14:02:57 +0100

    Printing names with less than 12 letters

--- directory.rb
+++ directory.rb
@@ -1,15 +1,14 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create and empty array, I like to use Array.new
+
   students = Array.new
-  #we ask for the first by using old gets.chomp
 
   name = $stdin.gets.chomp
   name.capitalize!
   puts "Joining which cohort?"
   cohort = $stdin.gets.chomp
-  # now we create the condition for our program to either store or finish
+
   while !name.empty? do
     students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
@@ -30,7 +29,7 @@ end
 
 def print(students)
   students.each_with_index() {|student, index|
-  puts "#{index.to_i + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].match /^A/ }
+  puts "#{index.to_i + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12 }
 end
 
 def print_footer(students)
