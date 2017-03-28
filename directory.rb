ff308c0d29202927dffb5a1ff2b4567991695313
Unai Motriko <unaimotriko@gmail.com>
2017-03-28 21:28:21 +0100

    ammending the cohort outmessage

--- directory.rb
+++ directory.rb
@@ -1,18 +1,18 @@
 def input_students
-  puts "Please enter the names of the students".center(20)
-  puts "To finish, just hit return twice".center(20)
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
 
   students = Array.new
 
   name = $stdin.gets.chomp
   name.capitalize!
-  puts "Please enter the age of the student:".center(20)
+  puts "Please enter the age of the student:"
   age = $stdin.gets.chomp
   age = "Age not provided" if age.empty?
-  puts "Please enter the nationality of the student:".center(20)
+  puts "Please enter the nationality of the student:"
   nationality = $stdin.gets.chomp
   nationality = "Nationality not provided" if nationality.empty?
-  puts "Joining which cohort?".center(20)
+  puts "Joining which cohort?"
   cohort = $stdin.gets.chomp
   cohort = "unknown" if cohort.empty?
   months = %w(january february march april may june july august september october november december unknown)
@@ -23,21 +23,26 @@ def input_students
 
   while !name.empty? do
     students << {name: name, age: age, nationality: nationality, cohort: cohort}
-    puts "Now we have #{students.count} students".center(20)
+    if students.count == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
+
     name = $stdin.gets.chomp
     name.capitalize!
      if !name.empty?
-       puts "Please enter the age of the student:".center(20)
+       puts "Please enter the age of the student:"
        age = $stdin.gets.chomp
          if age.empty?
            age = "Age not provided"
          end
-       puts "Please enter the nationality of the student:".center(20)
+       puts "Please enter the nationality of the student:"
        nationality = $stdin.gets.chomp
        if nationality.empty?
          nationality = "Nationality not provided"
        end
-       puts "Joining which cohort?".center(20)
+       puts "Joining which cohort?"
        cohort = $stdin.gets.chomp
        if cohort.empty?
          cohort = "Unkown"
@@ -49,17 +54,17 @@ def input_students
 end
 
 def print_header
-  puts "The students of Villains Academy".center(20)
-  puts "-------------".center(20)
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 def print(students)
   students.each_with_index() {|student, index|
-  puts "#{index.to_i + 1}. #{student[:name]}, #{student[:age]}, #{student[:nationality]} (#{student[:cohort]} cohort)".center(20)}
+  puts "#{index.to_i + 1}. #{student[:name]}, #{student[:age]}, #{student[:nationality]} (#{student[:cohort]} cohort)"}
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(20)
+  puts "Overall, we have #{students.count} great students"
 end
 
 students = input_students
