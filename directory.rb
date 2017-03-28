34f7f804fa15b3dc9556f4c62a8f888570022d95
Unai Motriko <unaimotriko@gmail.com>
2017-03-28 13:34:14 +0100

    User input; names & cohort

--- directory.rb
+++ directory.rb
@@ -17,29 +17,10 @@ def input_students
        puts "Joining which cohort?"
        cohort = $stdin.gets.chomp
      end
-
   end
   students
-
 end
 
-
-
-# previous script#
-#better store the students in an array
-#students = [
-#  {name: "Dr. Hannibal Lecter", cohort: :november},
-#  {name: "Darth Vader", cohort: :november},
-#  {name: "Nurse Ratched", cohort: :november},
-#  {name: "Michael Corleone", cohort: :november},
-#  {name: "Alex DeLarge", cohort: :november},
-#  {name: "The Wicked Witch of the West", cohort: :november},
-#  {name: "Terminator", cohort: :november},
-#  {name: "Freddy Krueger", cohort: :november},
-#  {name: "The Joker", cohort: :november},
-#  {name: "Joffrey Baratheon", cohort: :november},
-#  {name: "Norman Bates", cohort: :november}
-#]
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
